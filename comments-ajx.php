22b216fb797eaff23f41487adfdf0bfd12041705
Issam RACHDI <issam.rachdi@gmail.com>
2015-10-07 22:55:17 +0100

    ...

--- comments-ajx.php
+++ comments-ajx.php
@@ -1,4 +1,17 @@
 <?php
 
-//sqlite
+$sqlite = new SQLite3('db/comments.db');
+
+$action = $_GET['action'];
+
+if ($action == 'ADD_COMMENT') {
+  $name = $sqlite->escapeString($_GET['name']);
+  $email = $sqlite->escapeString($_GET['email']);
+  $page = $sqlite->escapeString($_GET['page']);
+  $comment = $sqlite->escapeString($_GET['comment']);
+
+  $sqlite->exec("INSERT INTO comments VALUES(NULL, '$name', '$email', '$comment', '$page')") or die('Erreur insertion commentaire');
+
+  echo 'OK';
+}
 
