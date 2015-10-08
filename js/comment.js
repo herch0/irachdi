5ae17412b07f0e50c33052fb3b1d3977a8d7fabe
Issam RACHDI <issam.rachdi@gmail.com>
2015-10-08 23:09:41 +0100

    ...

--- js/comment.js
+++ js/comment.js
@@ -1,27 +1,19 @@
-$(function () {
-  $("#add-comment").on('beforeSubmit', function (event) {
-    var form = $(this);
-    $.post(form.attr('action'), form.serialize())
-            .done(function (data) {
-              clear_comments_field();
-              load_comments(postId);
-            })
-            .fail(function () {
-              alert('Failed to add comment');
-            })
-    return false;
+window.onload = function () {
+  var page = location.href.substring(location.href.lastIndexOf('/') + 1);
+  document.querySelector("#add_comment").addEventListener('click', function () {
+    var name = document.querySelector("#name");
+    var email = document.querySelector("#email");
+    var comment = document.querySelector("#comment");
+
+    var form = document.querySelector("form");
+    var formData = new FormData(form);
+    
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", function() {
+      alert(this.responseText)
+    });
+    oReq.open("GET", "comments-ajx.php");
+    console.log(formData.get('name'));
+    oReq.send(new FormData(form));
   })
-  load_comments(postId);
-});
-
-function load_comments(post) {
-  $.get(baseUrl + '/comments/' + post, function (data) {
-    $("#comments_container").html(data);
-  });
-}
-
-function clear_comments_field() {
-  console.log('clearing comments');
-  $("#comment-author").val('');
-  $("#comment-content").val('');
 }
